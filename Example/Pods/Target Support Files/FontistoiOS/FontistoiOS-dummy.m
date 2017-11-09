c559fdd529dc470e1d1a6e8c3e57470e4776943d
Semih Emre ÜNLÜ <semihemreunlu@gmail.com>
2017-11-09 22:59:51 +0300

    Create bare project via pod create lib.

---
+++ Example/Pods/Target Support Files/FontistoiOS/FontistoiOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FontistoiOS : NSObject
+@end
+@implementation PodsDummy_FontistoiOS
+@end
---
+++ Example/Pods/Target Support Files/Pods-FontistoiOS_Example/Pods-FontistoiOS_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FontistoiOS_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FontistoiOS_Example
+@end
